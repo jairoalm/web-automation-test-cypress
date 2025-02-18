e69ea8ce87aa82211aa2055c82e0fe3a261d7f65
Jairo Almeida <jairoam85@gmail.com>
2025-02-18 14:29:19 -0300

    Configuração inicial do projeto

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -1,5 +1,8 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Central de Atendimento ao Cliente TAT', () => {
+  beforeEach(() => {
+    cy.visit('src/index.html')
+  });
+  it('verifica o título da aplicação', () => {
+    cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')    
   })
 })
