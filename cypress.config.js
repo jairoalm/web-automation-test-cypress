8916d9d07b48dd9c6ef884a9b47e806602c1e222
Jairo Almeida <jairoam85@gmail.com>
2025-05-29 11:50:16 -0300

    testando allure report

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,17 @@
-const { defineConfig } = require("cypress");
+import { defineConfig } from "cypress";
+import { allureCypress } from "allure-cypress/reporter";
 
-module.exports = defineConfig({
+export default defineConfig({
   viewportHeight: 880,
   viewportWidth: 1280,
-  e2e: {},
+  e2e: {
+    setupNodeEvents(on, config) {
+      allureCypress(on, config, {
+        resultsDir: "allure-results", // Define o diretório para os resultados do Allure
+      });
+      return config;      
+    },
+  },
   projectId: "u6uqez",
   video: true
 })
